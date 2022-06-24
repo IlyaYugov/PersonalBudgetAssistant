534411494a7a01004c5500f9175ba998f6f628da
IlyaYugov <ilyaugov@yandex.ru>
2022-06-24 19:33:06 +0700

    add project template

---
+++ PersonalBudgetAssistant/PersonalBudgetAssistant/PersonalBudgetAssistant/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using PersonalBudgetAssistant.ViewModels;
+using PersonalBudgetAssistant.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace PersonalBudgetAssistant
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ PersonalBudgetAssistant/PersonalBudgetAssistant/PersonalBudgetAssistant/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PersonalBudgetAssistant.Models;
+using PersonalBudgetAssistant.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PersonalBudgetAssistant.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
